3f2c49462bdf62973452c1969d4d5bb8218a9cd0
plrenaudin <pl.renaudin@gmail.com>
2016-09-11 21:56:21 +0100

    fix tests

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -15,7 +15,7 @@ describe('App.vue', () => {
     expect(vm.$el.querySelectorAll('.explorer li').length).toBe(1)
   })
   it('should render the editor title input', () => {
-    expect(vm.$el.querySelectorAll('.editor input.titleInput').length).toBe(1)
+    expect(vm.$el.querySelectorAll('input.titleInput').length).toBe(1)
   })
   it('should render the editor', () => {
     expect(vm.$el.querySelectorAll('.editorContent').length).toBe(1)
